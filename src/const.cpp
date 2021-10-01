4e672b321a5e0f88e7099832cda04a161f5f3a2f
Oleg 'livelace' Popov <o.popov@livelace.ru>
2021-10-02 00:28:29 +0300

    README.md

--- src/const.cpp
+++ src/const.cpp
@@ -9,7 +9,7 @@
 namespace defaults
 {
     extern const std::string APP_NAME = "clmixer";
-    extern const std::string APP_VERSION = "v1.0.0";
+    extern const std::string APP_VERSION = "v1.1.0";
     extern const std::string DEFAULT_OPENCL_KERNEL_NAME = "clmixer";
     extern const unsigned long DEFAULT_OPENCL_BUFFER_SIZE = 1 * 1024 * 1024 * 1024; // 1g
     extern const unsigned long DEFAULT_RANDOM_SEED = 42; // 42!
