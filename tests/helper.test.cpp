81ee12920d8a0a2b2098c8d34d61747b28a0ceb4
Oleg 'livelace' Popov <o.popov@livelace.ru>
2021-10-09 01:06:32 +0300

    ci

---
+++ tests/helper.test.cpp
@@ -0,0 +1,12 @@
+//
+// Created by user on 10/8/21.
+//
+
+#include <gtest/gtest.h>
+#include "helper.h"
+
+TEST(HelperTestSuite, HumanSize)
+{
+    EXPECT_EQ(human_readable(1024),"1K");
+    EXPECT_EQ(human_readable(10240),"10K");
+}
